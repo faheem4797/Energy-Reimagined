c433309efdfbdfabae7a824b2f5088150e50197c
Faheem <mfaheemamin4797@gmail.com>
2024-01-10 00:14:20 +0500

    edit blocs done

--- lib/features/admin/jobs/blocs/delete_job_bloc/delete_job_bloc.dart
+++ lib/features/admin/jobs/blocs/delete_job_bloc/delete_job_bloc.dart
@@ -1,13 +1,32 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:jobs_repository/jobs_repository.dart';
 
 part 'delete_job_event.dart';
 part 'delete_job_state.dart';
 
 class DeleteJobBloc extends Bloc<DeleteJobEvent, DeleteJobState> {
-  DeleteJobBloc() : super(DeleteJobInitial()) {
-    on<DeleteJobEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final JobsRepository _jobsRepository;
+  DeleteJobBloc({required JobsRepository jobsRepository})
+      : _jobsRepository = jobsRepository,
+        super(DeleteJobInitial()) {
+    on<JobDeleteRequested>(_jobDeleteRequested);
+  }
+
+  FutureOr<void> _jobDeleteRequested(
+      JobDeleteRequested event, Emitter<DeleteJobState> emit) async {
+    try {
+      await _jobsRepository.deleteJob(event.job);
+
+      emit(DeleteJobSuccess());
+    } on SetFirebaseDataFailure catch (e) {
+      emit(DeleteJobFailure(errorMessage: e.message));
+    } catch (e) {
+      log(e.toString());
+      emit(DeleteJobFailure(errorMessage: e.toString()));
+    }
   }
 }
--- lib/features/admin/jobs/blocs/delete_job_bloc/delete_job_event.dart
+++ lib/features/admin/jobs/blocs/delete_job_bloc/delete_job_event.dart
@@ -6,3 +6,11 @@ sealed class DeleteJobEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+final class JobDeleteRequested extends DeleteJobEvent {
+  final JobModel job;
+
+  const JobDeleteRequested({required this.job});
+  @override
+  List<Object> get props => [job];
+}
